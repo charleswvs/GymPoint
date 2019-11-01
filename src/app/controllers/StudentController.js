8dac49938d92eed70470fe64d7b23ef439efbd48
charles00willian <wcharles15@gmail.com>
2019-11-01 20:30:26 -0300

    authentication and user inserts

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,26 +1,40 @@
+/* eslint-disable class-methods-use-this */
 import Student from '../models/Student';
 
 class StudentController {
-  // eslint-disable-next-line class-methods-use-this
   async store(req, res) {
     // const { name, email, age } = req.body;
 
     const userExists = await Student.findOne({ where: { email: req.body.email } });
-    console.log(userExists);
+
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
     }
 
-
     const {
-      id, name, email, age,
+      id, name, email, age, weight, height,
     } = await Student.create(req.body);
 
     return res.json({
-      id, name, email, age,
+      id, name, email, age, weight, height,
     });
   }
 
+  async update(req, res) {
+    const student = await Student.findOne({ where: { email: req.body.email } });
+
+    if (!student) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    const {
+      id, name, email, age, weight, height,
+    } = await student.update(req.body);
+
+    return res.json({
+      id, name, email, age, weight, height,
+    });
+  }
   // async update(req, res){
   //   const
   // }
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 // test route
-routes.get('/', (req, res) => res.json({ hello: 'world' }));
+// routes.get('/', (req, res) => res.json({ hello: 'world' }));
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.post('/students', StudentController.store);
+routes.put('/students', StudentController.update);
 
 export default routes;
