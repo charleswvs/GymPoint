e2bdfb9759e50f2cdf267c9857179efb6325a9f5
charles00willian <wcharles15@gmail.com>
2019-11-01 07:47:57 -0300

    created student model

---
+++ src/database/migrations/20191101022628-create-users.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    // each object is column inside the table, and it's definition
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: (queryInterface) => queryInterface.dropTable('users'),
+};
---
+++ src/database/migrations/20191101023218-create-students.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // Permita que alunos sejam mantidos (cadastrados/atualizados) na aplicação
+  // utilizando nome, email, idade, peso e altura.
+  up: (queryInterface, Sequelize) => queryInterface.createTable('students', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    age: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: (queryInterface) => queryInterface.dropTable('users'),
+};
