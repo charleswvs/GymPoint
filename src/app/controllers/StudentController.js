6dccdee43cc9c0bdf77f2f7c659878bc9ee4fa3b
charles00willian <wcharles15@gmail.com>
2019-11-03 11:06:41 -0300

    Yup verifies typed info

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,9 +1,21 @@
 /* eslint-disable class-methods-use-this */
+import * as Yup from 'yup';
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().required(),
+      name: Yup.string().required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
     // const { name, email, age } = req.body;
+
+    if (!await schema.isValid(req.body)) {
+      return res.status(400).json({ error: 'Validation Failed' });
+    }
 
     const userExists = await Student.findOne({ where: { email: req.body.email } });
 
@@ -21,11 +33,31 @@ class StudentController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().required(),
+      newEmail: Yup.string(),
+      name: Yup.string(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!schema.isValid(req.body)) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const student = await Student.findOne({ where: { email: req.body.email } });
 
     if (!student) {
       return res.status(400).json({ error: 'User not found' });
     }
+
+    if (req.body.newEmail && req.body.newEmail === req.body.email
+      && await Student.findOne({ where: { email: req.body.newEmail } })) {
+      return res.status(400).json({ erro: 'Email is in use by other student' });
+    }
+
+    req.body.email = req.body.newEmail;
 
     const {
       id, name, email, age, weight, height,
