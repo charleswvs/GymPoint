82b4d805bd66d80fd0eb5e7e7c439c13856a44b0
charles00willian <wcharles15@gmail.com>
2019-11-14 18:30:16 -0300

    Fix: typo

--- src/database/migrations/20191101023218-create-students.js
+++ src/database/migrations/20191101023218-create-students.js
@@ -1,43 +1,44 @@
 module.exports = {
   // Permita que alunos sejam mantidos (cadastrados/atualizados) na aplicação
   // utilizando nome, email, idade, peso e altura.
-  up: (queryInterface, Sequelize) => queryInterface.createTable('students', {
-    id: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    age: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    },
-    weight: {
-      type: Sequelize.DOUBLE,
-      allowNull: false,
-    },
-    height: {
-      type: Sequelize.DOUBLE,
-      allowNull: false,
-    },
-    created_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-    updated_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-  }),
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      weight: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      height: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
 
-  down: (queryInterface) => queryInterface.dropTable('users'),
+  down: queryInterface => queryInterface.dropTable('users'),
 };
