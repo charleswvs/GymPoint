6207e838d75950136d53f99e6dfc7e8f516ec376
charles00willian <wcharles15@gmail.com>
2019-11-19 13:36:39 -0300

    created registration model

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,13 @@
 import Sequelize from 'sequelize';
 import User from '../app/models/User';
-import databaseConfig from '../config/database';
+
 import Student from '../app/models/Student';
 import Plan from '../app/models/Plan';
+import Registration from '../app/models/Registration';
 
-const models = [User, Student, Plan];
+import databaseConfig from '../config/database';
+
+const models = [User, Student, Plan, Registration];
 
 class Database {
   constructor() {
@@ -15,7 +18,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
