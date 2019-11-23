ea9a8f057a4939e600ec63174acd53d2b1e42b5d
charles00willian <wcharles15@gmail.com>
2019-11-23 17:07:08 -0300

    creating registrations and sending emails

--- src/app/controllers/RegistrationController.js
+++ src/app/controllers/RegistrationController.js
@@ -1,6 +1,11 @@
 import { parseISO } from 'date-fns';
+import * as Yup from 'yup';
+
+import Mail from '../../lib/Mail';
+
 import Registration from '../models/Registration';
 import Plan from '../models/Plan';
+import Student from '../models/Student';
 
 class RegistrationController {
   async index(req, res) {
@@ -8,9 +13,21 @@ class RegistrationController {
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      student_id: Yup.number().required(),
+      plan_id: Yup.number().required(),
+      start_date: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const { student_id, plan_id, start_date } = req.body;
 
     const { duration, price } = await Plan.findByPk(plan_id);
+
+    const { name, email } = await Student.findByPk(student_id);
 
     const end_date = await parseISO(start_date);
     end_date.setMonth(end_date.getMonth() + duration);
@@ -20,7 +37,14 @@ class RegistrationController {
       plan_id,
       start_date,
       end_date,
-      price: price + duration,
+      price: price * duration,
+    });
+
+    // Basic email sending
+    await Mail.sendMail({
+      to: `${name} <${email}>`,
+      subject: 'Registration created',
+      text: 'Your registration to GymPoint has been sucessefully created',
     });
 
     return res.json(registration);
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secured: false,
+  auth: {
+    user: '4de4c4408720a1',
+    pass: '92995718738eb8',
+  },
+  default: {
+    from: 'Equipe GymPoint <noreply@gympoint.com>',
+  },
+};
